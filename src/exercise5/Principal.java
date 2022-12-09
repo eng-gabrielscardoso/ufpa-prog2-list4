ced94b58786071afccf8dc06725a0442477cbe29
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-12-08 21:16:35 -0300

    supress warnings on exercise 5

--- src/exercise5/Principal.java
+++ src/exercise5/Principal.java
@@ -3,6 +3,7 @@ package exercise5;
 public class Principal {
 	public static void main(String[] args) {
 		Circulo c = new Circulo(3);
+		@SuppressWarnings("unused")
 		Quadrado q = new Quadrado(4);
 		
 		c.imprimeDados();
